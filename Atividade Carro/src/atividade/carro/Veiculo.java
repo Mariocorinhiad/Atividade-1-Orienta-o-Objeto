bb563f53fb018475c4dae4b0666380f24eb6306f
Mario Levino <144383929+Mariocorinhiad@users.noreply.github.com>
2024-02-29 22:44:09 -0300

    Add files via upload

---
+++ Atividade Carro/src/atividade/carro/Veiculo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atividade.carro;
+
+/**
+ *
+ * @author Admin
+ */
+public class Veiculo {
+    
+    public static String Marca = "Chevrolet";
+    
+    public int ano;  
+    public String Modelo;
+    public String Cor;  
+    
+    public Veiculo(int ano,String Modelo,String Cor){
+        
+        this.ano = ano;
+        this.Modelo = Modelo;
+        this.Cor = Cor;
+    }
+}
